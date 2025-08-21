18bf35988e17f72af0b1d62ca85293d294e04ab4
Rachit Bhatta <rachitbhatta2009@gmail.com>
2025-08-21 16:50:16 +0545

    Todo with redux

---
+++ 11-veet/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import AddTodo from './components/addTodo'
+import Todos from './components/Todo'
+
+function App() {
+
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
